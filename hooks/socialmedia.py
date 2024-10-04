c34bc24631cad933fbfe257f4689f191dcda2248
Estela Ribeiro <44876834+rEstela@users.noreply.github.com>
2024-10-04 09:40:54 -0300

    Create socialmedia.py

---
+++ hooks/socialmedia.py
@@ -0,0 +1,23 @@
+from textwrap import dedent
+import urllib.parse
+import re
+
+x_intent = "https://twitter.com/intent/tweet"
+fb_sharer = "https://www.facebook.com/sharer/sharer.php"
+linkedin_sharer = "https://www.linkedin.com/sharing/share-offsite/"
+include = re.compile(r"blog/[1-9].*")
+
+def on_page_markdown(markdown, **kwargs):
+    page = kwargs['page']
+    config = kwargs['config']
+    if not include.match(page.url):
+        return markdown
+
+    page_url = config.site_url+page.url
+    page_title = urllib.parse.quote(page.title+'\n')
+
+    return markdown + dedent(f"""
+    [Share on :simple-x:]({x_intent}?text={page_title}&url={page_url}){{ .md-button }}
+    [Share on :simple-facebook:]({fb_sharer}?u={page_url}){{ .md-button }}
+    [Share on :simple-linkedin:]({linkedin_sharer}?url={page_url}){{ .md-button }}
+    """)
